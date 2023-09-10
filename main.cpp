d3a2f6ee1d720ef4627756723bad6eb84741d12e
hear-the-wind-sing <2514872803@qq.com>
2023-09-10 19:46:53 +0800

    Bresenham’s Line Drawing Algorithm

--- main.cpp
+++ main.cpp
@@ -2,14 +2,69 @@
 
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
-
+void line_float(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
+    bool steep = false; 
+    if (std::abs(x0-x1)<std::abs(y0-y1)) { 
+        std::swap(x0, y0); 
+        std::swap(x1, y1); 
+        steep = true; 
+    } 
+    if (x0>x1) { 
+        std::swap(x0, x1); 
+        std::swap(y0, y1); 
+    } 
+    int dx = x1-x0; 
+    int dy = y1-y0; 
+    float derror = std::abs(dy/float(dx)); 
+    float error = 0; 
+    int y = y0; 
+    for (int x=x0; x<=x1; x++) { 
+    	printf("x:%d,y:%d,Y:%f,error:%f\n",x,y,y+error,error);
+        if (steep) { 
+            image.set(y, x, color); 
+        } else { 
+            image.set(x, y, color); 
+        } 
+        error += derror; 
+        if (error>.5) { 
+            y += (y1>y0?1:-1); 
+            error -= 1.; 
+        } 
+    } 
+} 
+void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) { 
+    bool steep = false; 
+    if (std::abs(x0-x1)<std::abs(y0-y1)) { 
+        std::swap(x0, y0); 
+        std::swap(x1, y1); 
+        steep = true; 
+    } 
+    if (x0>x1) { 
+        std::swap(x0, x1); 
+        std::swap(y0, y1); 
+    } 
+    int dx = x1-x0; 
+    int dy = y1-y0; 
+    int derror2 = std::abs(dy)*2; 
+    int error2 = 0; 
+    int y = y0; 
+    for (int x=x0; x<=x1; x++) { 
+        if (steep) { 
+            image.set(y, x, color); 
+        } else { 
+            image.set(x, y, color); 
+        } 
+        error2 += derror2; 
+        if (error2 > dx) { 
+            y += (y1>y0?1:-1); 
+            error2 -= dx*2; 
+        } 
+    } 
+}
 int main(int argc, char** argv) {
 	TGAImage image(100, 100, TGAImage::RGB);
-	image.set(52, 41, red); 
-	image.set(52, 40, white);
-	image.set(51, 40, red); 
-	image.set(52, 39, red); 
-	image.set(53, 40, red); 
+	line_float(0,0,200,150,image,red);
+	line(10,30,300,150,image,white);
 	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
 	image.write_tga_file("output.tga");
 	return 0;
